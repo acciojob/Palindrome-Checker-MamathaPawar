657d9a230c29cc604fc8de3f118f14e7d8c3ce62
MamathaPawar <103501747+MamathaPawar@users.noreply.github.com>
2023-03-20 19:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
-function palindrome(str){
 
+function palindrome(str) {
+  
+  const cleanStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+  
+  return cleanStr === cleanStr.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
